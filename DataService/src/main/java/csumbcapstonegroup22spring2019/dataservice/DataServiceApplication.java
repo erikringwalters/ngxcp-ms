1e41122ef27fe141a202254654c38c69abe92932
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-03-30 00:20:48 -0700

    eureka configs

--- DataService/src/main/java/csumbcapstonegroup22spring2019/dataservice/DataServiceApplication.java
+++ DataService/src/main/java/csumbcapstonegroup22spring2019/dataservice/DataServiceApplication.java
@@ -2,7 +2,9 @@ package csumbcapstonegroup22spring2019.dataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DataServiceApplication {
 
--- DataService/src/main/java/csumbcapstonegroup22spring2019/dataservice/api/DataController.java
+++ DataService/src/main/java/csumbcapstonegroup22spring2019/dataservice/api/DataController.java
@@ -1,5 +1,6 @@
 package csumbcapstonegroup22spring2019.dataservice.api;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import csumbcapstonegroup22spring2019.dataservice.api.entities.AlterEgo;
 import csumbcapstonegroup22spring2019.dataservice.api.entities.Hero;
+import csumbcapstonegroup22spring2019.dataservice.api.entities.Identity;
 import csumbcapstonegroup22spring2019.dataservice.business.Manager;
+import csumbcapstonegroup22spring2019.dataservice.api.entities.Powers;
+
 
 @RestController
 public class DataController{
@@ -19,6 +24,11 @@ public class DataController{
     @GetMapping("/Data")
     @ResponseBody
     List<Hero> getData () {
-        return null;
+        Hero xavier = new Hero("1", new Identity("Charles Xavier"), new AlterEgo("Professor X"), new Powers(Arrays.asList("mind stuff")));
+       Hero wolverine = new Hero("2", new Identity("James Howlett"), new AlterEgo("Wolverine"), new Powers(Arrays.asList("strength","rapid healing","metal bones")));
+       Hero cyclops = new Hero("3", new Identity("Scott Summers"), new AlterEgo("Cyclops"), new Powers(Arrays.asList("eye beams")));
+       List<Hero> result = Arrays.asList(xavier, wolverine, cyclops);
+       // List<Hero> result = callDB();
+       return result;
     }
 }
